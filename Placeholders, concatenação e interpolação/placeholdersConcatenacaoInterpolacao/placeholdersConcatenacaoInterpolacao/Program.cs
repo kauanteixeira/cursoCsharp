7cf9542a0e49f1e795487b6d5383dee738329c28
kauanteixeira <kauant.henrique@gmail.com>
2025-05-18 21:33:58 -0300

    Aprendo sobre tipo de dados e formatação de dados

---
+++ Placeholders, concatenação e interpolação/placeholdersConcatenacaoInterpolacao/placeholdersConcatenacaoInterpolacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Kauan";
+            int idade = 19;
+            double saldo = 10.35624;
+
+            //Placeholder
+            Console.WriteLine("{0} tem {1} anos e possui o saldo de {2:F2} na conta!", nome, idade, saldo);
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e possui o saldo de {saldo} na conta!");
+
+
+            //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos" + " e possui o saldo de " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " na conta!");
+
+        }
+    }
+}
---
+++ Saida de Dados/saidaDeDados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'M';
+            string nome = "Kauan";
+            int idade = 19;
+            double saldo = 10.129;
+
+            //Diferença entre WriteLine e Write
+
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("--------------------");
+
+            Console.Write("Bom dia!");
+            Console.Write("Boa tarde!");
+            Console.Write("Boa noite!");
+            Console.WriteLine("--------------------");
+
+            //Imprimindo dados e formatação de dados
+
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2")); //ToString para definir quantas casas decimais aparece após a , ou .
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); //CultureInfo.InvariantCulture para deixar a formatação padrão, ignora a formatação do pais
+
+        }
+    }
+}
