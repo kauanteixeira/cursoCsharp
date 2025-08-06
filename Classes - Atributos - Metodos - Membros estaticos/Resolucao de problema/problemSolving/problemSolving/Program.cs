6499d5874fa5c695cade7f884b2a84562254677f
kauanteixeira <kauant.henrique@gmail.com>
2025-08-05 22:22:28 -0300

    Melhorando a estrutura de pastas e inserindo uma resolução de exercício

---
+++ Classes - Atributos - Metodos - Membros estaticos/Resolucao de problema/problemSolving/problemSolving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            double xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            double yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = CalculaArea(xA, xB, xC);
+            double areaY = CalculaArea(yA, yB, yC);
+
+            Console.WriteLine($"Area de X: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Area de Y: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if ( areaX > areaY)
+                Console.WriteLine($"Maior area: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            else
+                Console.WriteLine($"Maior area: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+
+        static double CalculaArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a)* (p - b) * (p - c));
+            return area;
+        }
+    }
+}
