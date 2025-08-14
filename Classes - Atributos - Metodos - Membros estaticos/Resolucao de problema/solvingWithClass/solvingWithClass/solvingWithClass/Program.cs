1b773e9edaea023d674e0fca13054b23ad40dc33
kauanteixeira <kauant.henrique@gmail.com>
2025-08-14 17:55:20 -0300

    Adicionando a resolução do exercício do triangulo usando classes

---
+++ Classes - Atributos - Metodos - Membros estaticos/Resolucao de problema/solvingWithClass/solvingWithClass/solvingWithClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using solvingWithClass;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle x, y;
+            x = new Triangle();
+            y = new Triangle();
+
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = CalculaArea(x.A, x.B, x.C);
+            double areaY = CalculaArea(y.A, y.B, y.C);
+
+            Console.WriteLine($"Area de X: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Area de Y: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (areaX > areaY)
+                Console.WriteLine($"Maior area: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            else
+                Console.WriteLine($"Maior area: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+
+        static double CalculaArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+    }
+}
