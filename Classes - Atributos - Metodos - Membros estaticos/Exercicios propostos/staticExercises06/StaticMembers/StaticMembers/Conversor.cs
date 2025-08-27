49a5b901df833d5ef00a92f737e94eb5e2ba1b42
kauanteixeira <kauant.henrique@gmail.com>
2025-08-26 21:21:04 -0300

    Resolvendo um exercício utilizando membros estáticos

---
+++ Classes - Atributos - Metodos - Membros estaticos/Exercicios propostos/staticExercises06/StaticMembers/StaticMembers/Conversor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticMembers
+{
+    public class Conversor
+    {
+        public static double Iof = 6.0;
+        public static double DolarReal(double cotacao, double quantia)
+        {
+            return quantia * cotacao * (1 + Iof / 100);
+        }
+    }
+}
---
+++ Classes - Atributos - Metodos - Membros estaticos/Exercicios propostos/staticExercises06/StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using StaticMembers;
+
+namespace MyCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor a ser pago em reais = R$ {Conversor.DolarReal(cotacao, quantia).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
