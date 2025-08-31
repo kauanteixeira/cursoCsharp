2eff09bc37b2109224b15864da6c2ba9227be865
kauanteixeira <kauant.henrique@gmail.com>
2025-08-31 16:04:30 -0300

    Exemplo utilizando sobrecarga de métodos

---
+++ Construtores - This - Sobrecarga - Encapsulamento/Sobrecarga/Sobrecarga/Sobrecarga/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Sobrecarga;
+
+namespace MyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
