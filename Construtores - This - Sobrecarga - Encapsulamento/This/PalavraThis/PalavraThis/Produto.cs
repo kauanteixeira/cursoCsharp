6316603792b733520d89efd66ced2360a994a197
kauanteixeira <kauant.henrique@gmail.com>
2025-08-31 17:29:14 -0300

    feat: adicionar o uso da palavra this para referenciar o objeto atual dentro de uma classe

---
+++ Construtores - This - Sobrecarga - Encapsulamento/This/PalavraThis/PalavraThis/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace PalavraThis
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtde;
+
+        /*
+            This para diferenciar atributos de variaveis locais, e referênciar outro construtor em um construtor.
+        */
+        public Produto ()
+        {
+            Qtde = 3;
+        }
+        public Produto(string Nome, double Preco) : this()
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Qtde * Preco;
+        }
+        public void AdicionarProdutos(int value)
+        {
+            Qtde += value;
+        }
+        public void RemoverProdutos(int value)
+        {
+            Qtde -= value;
+        }
+        public override string ToString()
+        {
+            return
+                Nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Qtde
+                + " unidade"
+                + ", Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)
+            ;
+        }
+
+
+    }
+}
