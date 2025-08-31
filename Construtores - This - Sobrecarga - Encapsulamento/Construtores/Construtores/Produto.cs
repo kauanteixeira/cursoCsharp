c18172a90894ff78997407feb4bba5244bc9b99d
kauanteixeira <kauant.henrique@gmail.com>
2025-08-31 09:10:37 -0300

    Adicionando o aprendizado sobre Construtores

---
+++ Construtores - This - Sobrecarga - Encapsulamento/Construtores/Construtores/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtde;
+
+        public Produto(string nome, double preco, int qtde)
+        {
+            Nome = nome;
+            Preco = preco;
+            Qtde = qtde;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Qtde * Preco;
+        }
+        public void AdicionarProdutos(int value)
+        {
+            Qtde += value;
+        }
+        public void RemoverProdutos(int value)
+        {
+            Qtde -= value;
+        }
+        public override string ToString()
+        {
+            return
+                Nome
+                + ", R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Qtde
+                + " unidade"
+                + ", Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)
+            ;
+        }
+
+
+    }
+}
---
+++ Construtores - This - Sobrecarga - Encapsulamento/Construtores/Construtores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Construtores;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, qtde);
+
+            Console.WriteLine($"Dados do produto: {p}");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int v = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(v);
+
+            Console.WriteLine($"Dados atualizados: {p}");
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            int v1 = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(v1);
+
+            Console.WriteLine($"Dados atualizados: {p}");
+        }
+    }
+}
