65a1008f931ed38004c1479da3c1632d46228ea5
kauanteixeira <kauant.henrique@gmail.com>
2025-08-31 22:53:09 -0300

    feat: implementar o uso de properties get/set

---
+++ Construtores - This - Sobrecarga - Encapsulamento/Propriedades/Propriedades/Propriedades/Propriedades.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace Propriedades
+{
+    public class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _qtde;
+
+        public Produto(string nome, double preco, int qtde)
+        {
+            _nome = nome;
+            _preco = preco;
+            _qtde = qtde;
+        }
+
+        public string Nome
+        {
+            get{ return _nome; }
+            set 
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public int Qtde
+        {
+            get { return _qtde; }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _qtde * _preco;
+        }
+        public void AdicionarProdutos(int value)
+        {
+            _qtde += value;
+        }
+        public void RemoverProdutos(int value)
+        {
+            _qtde -= value;
+        }
+        public override string ToString()
+        {
+            return
+                _nome
+                + ", R$"
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _qtde
+                + " unidade"
+                + ", Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)
+            ;
+        }
+
+
+    }
+}
