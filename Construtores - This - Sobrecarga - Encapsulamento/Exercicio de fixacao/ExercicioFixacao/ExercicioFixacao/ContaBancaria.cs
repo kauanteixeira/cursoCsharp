ed25738e83df9bfcacb6598729238759e0cdc247
kauanteixeira <kauant.henrique@gmail.com>
2025-09-04 22:48:21 -0300

    feat: resolução do exercício de fixação

---
+++ Construtores - This - Sobrecarga - Encapsulamento/Exercicio de fixacao/ExercicioFixacao/ExercicioFixacao/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conta
+{
+    public class ContaBancaria
+    {
+        private int _numero;
+        public string Titular;
+        public double Saldo { get; private set; }
+
+        public int NumConta(int numero)
+        {
+            return this._numero = numero;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return 
+                "Conta: " 
+                + _numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Construtores - This - Sobrecarga - Encapsulamento/Exercicio de fixacao/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using Conta;
+
+namespace MyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cliente = new ContaBancaria();
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.NumConta(numero);
+
+            Console.Write("Entre o titular da conta: ");
+            cliente.Titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string operacao = Console.ReadLine();
+
+            if (operacao.ToLower() == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                cliente.Deposito(depositoInicial);
+                Console.WriteLine(cliente);
+            }
+            else
+            {
+                Console.WriteLine(cliente);
+            }
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            double quantiaDeposito = double.Parse(Console.ReadLine());
+            cliente.Deposito(quantiaDeposito);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+
+            Console.WriteLine("Entre um valor para saque: ");
+            double quantiaSaque = double.Parse(Console.ReadLine());
+            cliente.Saque(quantiaSaque);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+        }
+    }
+}
